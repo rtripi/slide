64322ed19f280d22e11731add1e14c82274fe0e3
Renato Tripi <renatosft@gmail.com>
2021-10-25 14:30:15 -0300

    slide next prev added

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ import Slide from "./slide.js";
 
 const slide = new Slide(".slide", ".slide-wrapper");
 slide.init();
+console.log(slide);
 
-slide.slidesIndexNav();
+slide.changeSlide(3);
+slide.activePrevSlide();
